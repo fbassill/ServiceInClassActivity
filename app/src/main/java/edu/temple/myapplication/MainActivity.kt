3d5b8f4a817a86c8659163f76fcdd7f41277b438
finnbassill <tun39698@temple.edu>
2023-11-28 11:52:36 -0500

    Created menu for timer service actions

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,13 +8,15 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var  timerTextView: TextView
+    private lateinit var  timerTextView: TextView
     lateinit var timerBinder : TimerService.TimerBinder
     var isConnected = false
 
@@ -24,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         true
     }
 
-    val serviceConnection = object : ServiceConnection {
+    private val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             timerBinder = service as TimerService.TimerBinder
             timerBinder.setHandler(timerHandler)
@@ -34,6 +36,22 @@ class MainActivity : AppCompatActivity() {
         override fun onServiceDisconnected(name: ComponentName?) {
             isConnected = false
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.startTimer-> if (isConnected) timerBinder.start(100)
+            R.id.stopTimer -> if (isConnected) timerBinder.stop()
+            R.id.pauseTimer -> if (isConnected) timerBinder.pause()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,6 +68,8 @@ class MainActivity : AppCompatActivity() {
             BIND_AUTO_CREATE
         )
 
+
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
             if (isConnected) timerBinder.start(100)
         }
@@ -57,9 +77,13 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
             if (isConnected) timerBinder.pause()
         }
-        
+
         findViewById<Button>(R.id.stopButton).setOnClickListener {
             if (isConnected) timerBinder.stop()
         }
+
+
     }
+
+    //backPressCallback.isEnabled = true
 }
